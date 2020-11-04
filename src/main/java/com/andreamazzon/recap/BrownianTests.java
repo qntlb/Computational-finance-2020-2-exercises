09c59f85ef48492eedaa9f585da474c04a94a938
Andrea Mazzon <mazzon@math.lmu.de>
2020-11-04 14:41:41 +0100

    Code for some recap about Brownian motions and valuation of options

---
+++ src/main/java/com/andreamazzon/recap/BrownianTests.java
@@ -0,0 +1,138 @@
+package com.andreamazzon.recap;
+
+import java.text.DecimalFormat;
+
+import net.finmath.montecarlo.BrownianMotion;
+import net.finmath.montecarlo.BrownianMotionFromMersenneRandomNumbers;
+import net.finmath.montecarlo.RandomVariableFromDoubleArray;
+import net.finmath.stochastic.RandomVariable;
+import net.finmath.time.TimeDiscretization;
+import net.finmath.time.TimeDiscretizationFromArray;
+
+/**
+ *
+ * This class gives an overview of the simulation of the Brownian Motion with the Finmath library, besides some
+ * other functionalities.
+ *
+ * @author: Andrea Mazzon
+ *
+ */
+
+public final class BrownianTests {
+
+	/*
+	 * note: how to set some printing settings. We use objects of type DecimalFormat, which is a class that
+	 * extends DecimalFormat. At the right, you see the constructor.
+	 */
+	static final DecimalFormat FORMATTERPOSITIVE2 = new DecimalFormat("0.00");
+	static final DecimalFormat FORMATTERREAL4 = new DecimalFormat(" 0.0000;-0.0000");
+
+	public static void main(final String[] args) {
+
+
+		// The parameters
+		final int numberOfPaths = 100000;//i.e., the number of simulated trajectories
+		final int seed = 1897;//this is the seed that we need to generate the random numbers
+		final double firstTime = 0.0;
+		final double lastTime = 1.0;
+		final double dt = 0.01;//the time step
+
+		// Time discretization, number of steps deduced from lastTime and dt
+		final TimeDiscretization timeDiscretization = new TimeDiscretizationFromArray(
+				firstTime, //first time of the time discretization
+				(int) (lastTime / dt),//number of times
+				dt //time step
+				);
+
+		// We generate a Two-dimensional Brownian motion
+		final BrownianMotion brownian = new BrownianMotionFromMersenneRandomNumbers(
+				timeDiscretization, //the time discretization of the Brownian motion
+				2, // number of independent Brownian motions that we generate: this is a 2-dimensional Brownian Motion
+				numberOfPaths,//number of simulated paths
+				seed //the seed that is needed to generate the Mersenne random numbers (see line 141 of BrownianMotionFromMersenneRandomNumbers)
+				);
+
+		System.out.println("Average, variance and other properties of a BrownianMotion."
+				+ "\n Time step size (dt): " + dt + "  Number of path: " + numberOfPaths + "\n");
+		System.out.println("      " + "\t" + "  int dW_1 " + "\t" + "int dW_1 dW_1" + "\t" + "int dW_1 dW_2" + "\t");
+		System.out.println("time" + "\t" + " mean" + "\t" + " var" + "\t" + " mean" + "\t" + " var" + "\t" + " mean"
+				+ "\t" + " var");
+
+		final int numberOfTimes = timeDiscretization.getNumberOfTimes();
+
+		//note: arrays of RandomVariable objects
+		final RandomVariable[] firstBrownianMotionPath = new RandomVariable[numberOfTimes];
+		final RandomVariable[] secondBrownianMotionPath = new RandomVariable[numberOfTimes];
+
+		final RandomVariable[] firstQuadraticVariationPath = new RandomVariable[numberOfTimes];
+		final RandomVariable[] quadraticCovariationPath = new RandomVariable[numberOfTimes];
+
+
+		// We set B_0 = 0. Note that this is a deterministic constant
+		firstBrownianMotionPath[0] = new RandomVariableFromDoubleArray(0.0 /*the time*/, 0.0 /*the value*/);
+		secondBrownianMotionPath[0] = new RandomVariableFromDoubleArray(0.0 /*the time*/, 0.0 /*the value*/);
+		firstQuadraticVariationPath[0] = new RandomVariableFromDoubleArray(0.0 /*the time*/, 0.0 /*the value*/);
+		quadraticCovariationPath[0] = new RandomVariableFromDoubleArray(0.0 /*the time*/, 0.0 /*the value*/);
+
+		for (int timeIndex = 1; timeIndex < timeDiscretization.getNumberOfTimeSteps(); timeIndex++) {
+
+			// We get W(t+dt) from dW(t)
+
+			//first path
+			firstBrownianMotionPath[timeIndex] = firstBrownianMotionPath[timeIndex - 1]
+					.add(brownian.getBrownianIncrement(timeIndex, 0));
+			//second path
+			secondBrownianMotionPath[timeIndex] = secondBrownianMotionPath[timeIndex - 1]
+					.add(brownian.getBrownianIncrement(timeIndex, 1));
+
+
+			// We compute the quadratic variation of the first path
+			firstQuadraticVariationPath[timeIndex] = firstQuadraticVariationPath[timeIndex - 1]
+					.add(brownian.getBrownianIncrement(timeIndex, 0).squared());
+
+			// We compute the quadratic covariation of the two paths
+			quadraticCovariationPath[timeIndex] = quadraticCovariationPath[timeIndex - 1]
+					.add(brownian.getBrownianIncrement(timeIndex, 0).mult(brownian.getBrownianIncrement(timeIndex, 1)));
+
+			/*
+			 * we print the results. Note the use of the NumberFormat.format method. And of course the
+			 * getAverage() method of RandomVariable.
+			 */
+			System.out.println(FORMATTERPOSITIVE2.format(timeDiscretization.getTime(timeIndex)) + "\t"
+					+ FORMATTERREAL4.format(firstBrownianMotionPath[timeIndex].getAverage()) + "\t"
+					+ FORMATTERREAL4.format(firstBrownianMotionPath[timeIndex].getVariance()) + "\t"
+					+ FORMATTERREAL4.format(firstQuadraticVariationPath[timeIndex].getAverage()) + "\t"
+					+ FORMATTERREAL4.format(firstQuadraticVariationPath[timeIndex].getVariance()) + "\t"
+					+ FORMATTERREAL4.format(quadraticCovariationPath[timeIndex].getAverage()) + "\t"
+					+ FORMATTERREAL4.format(quadraticCovariationPath[timeIndex].getVariance()) + "\t" + "");
+		}
+		System.out.println("\n");
+
+		/*
+		 * if the tables above confuse you, one can just pick a given time and analyse
+		 * statistic by statistic, since we stored everything in an array:
+		 */
+		final double time = 0.5;
+		final int indexForTheGivenTime = timeDiscretization.getTimeIndexNearestGreaterOrEqual(time);
+
+
+		// mean and variance of the Brownian motion at the given time
+		System.out.println("Mean of the first Brownian Motion at time " + time + " : "
+				+ firstBrownianMotionPath[indexForTheGivenTime].getAverage());
+		System.out.println("Variance of the first Brownian Motion at time " + time + " : "
+				+ secondBrownianMotionPath[indexForTheGivenTime].getVariance() + "\n");
+
+
+		// mean and variance of the quadratic variation of the Brownian motion at the given time
+		System.out.println("Mean of the Quadratic Variation of the first Brownian motion at time " + time + " : "
+				+ firstQuadraticVariationPath[indexForTheGivenTime].getAverage());
+		System.out.println("Variance of the Quadratic Variation of the first Brownian motion at time " + time + " : "
+				+ firstQuadraticVariationPath[indexForTheGivenTime].getVariance()+ "\n");
+
+		// mean and variance of the quadratic variation of the tqo Brownian motions at the given time
+		System.out.println("Mean of the Quadratic Covariation of the two Brownian motions at time " + time + " : "
+				+ quadraticCovariationPath[indexForTheGivenTime].getAverage());
+		System.out.println("Variance of the Quadratic Covariation of the two Brownian motions at time " + time + " : "
+				+ quadraticCovariationPath[indexForTheGivenTime].getVariance());
+	}
+}
---
+++ src/main/java/com/andreamazzon/recap/CallWithFinmath.java
@@ -0,0 +1,116 @@
+package com.andreamazzon.recap;
+
+
+import java.text.DecimalFormat;
+
+import net.finmath.exception.CalculationException;
+import net.finmath.functions.AnalyticFormulas;
+import net.finmath.montecarlo.BrownianMotion;
+import net.finmath.montecarlo.BrownianMotionFromMersenneRandomNumbers;
+import net.finmath.montecarlo.assetderivativevaluation.AssetModelMonteCarloSimulationModel;
+import net.finmath.montecarlo.assetderivativevaluation.MonteCarloBlackScholesModel;
+import net.finmath.montecarlo.assetderivativevaluation.products.AbstractAssetMonteCarloProduct;
+import net.finmath.montecarlo.assetderivativevaluation.products.EuropeanOption;
+import net.finmath.time.TimeDiscretization;
+import net.finmath.time.TimeDiscretizationFromArray;
+
+/**
+ * The main method of this class computes the value of a call option by Monte Carlo
+ * using the classes of the Finmath library. It also computes the analytical value and then the
+ * absolute error. It does the same using the same model, but with a modified seed to generate the
+ * random numbers.
+ *
+ * Here we see:
+ *
+ * - how to link model and process to produce an object representing a Monte-Carlo simulation
+ * (ok, we don't see it directly here, but having a look at the Finmath class);
+ * - how to construct an object of type MonteCarloBlackScholesModel, in two ways (constructing a Brownian motion
+ * or only giving the parameters)
+ * - how to pass such an object to an AbstractAssetMonteCarloProduct class in order to evaluate an option;
+ * - how to get the same object with modified seed (or modified data in general).
+ *
+ *
+ * @author Andrea Mazzon
+ *
+ */
+public class CallWithFinmath {
+
+	static final DecimalFormat FORMATTERPOSITIVE4 = new DecimalFormat("0.0000");
+
+	public static void main(String[] args) throws CalculationException {
+
+		//process parameters
+		final double initialPrice = 100.0;
+		final double volatility = 0.25; //the volatility of the underlying
+		final double riskFreeRate = 0;
+
+		//option parameters
+		final double strike = 100.0;
+		final double maturity = 1.0;
+
+		//simulation parameter
+		final int numberOfSimulations = 100000;//the number of paths simulated
+
+		//time discretization parameters
+		final double initialTime = 0;
+		final int numberOfTimeSteps = 100;
+		final double timeStep = maturity / numberOfTimeSteps;
+		final TimeDiscretization times = new TimeDiscretizationFromArray(initialTime,
+				numberOfTimeSteps, timeStep);
+
+		/*
+		 * look at the class: it links together the model, i.e., the specification of the dynamics
+		 * of the underlying, and the process, i.e., the discretization of the paths.
+		 */
+		final AssetModelMonteCarloSimulationModel bsModel = new MonteCarloBlackScholesModel(
+				times, numberOfSimulations, initialPrice, riskFreeRate, volatility);
+		final AbstractAssetMonteCarloProduct europeanOption = new EuropeanOption(maturity, strike);
+
+		//have a look at this class!
+		final double analyticValue = AnalyticFormulas.blackScholesOptionValue(
+				initialPrice, riskFreeRate, volatility, maturity, strike);
+
+		//note the getValue method: where is getValue(0.0, model) implemented?
+		final double monteCarloValue = europeanOption.getValue(bsModel);
+
+		System.out.println("B-S Monte Carlo value: " + FORMATTERPOSITIVE4.format(monteCarloValue)
+		+ "\n" + "Analytical value: " + FORMATTERPOSITIVE4.format(analyticValue) + "\n" + "Absolute percentage error: "
+		+ FORMATTERPOSITIVE4.format(Math.abs(analyticValue-monteCarloValue)/analyticValue*100)+ "\n" );
+
+		//now we want to do the same with a new seed
+		final int newSeed = 1897;
+
+		final AssetModelMonteCarloSimulationModel bsModelWithModifiedDrift = bsModel.getCloneWithModifiedSeed(newSeed);
+
+		final double newMonteCarloValue = europeanOption.getValue(bsModelWithModifiedDrift);
+
+		System.out.println("New B-S Monte Carlo value: " + FORMATTERPOSITIVE4.format(newMonteCarloValue)
+		+ "\n" + "New absolute percentage error: "
+		+ FORMATTERPOSITIVE4.format(Math.abs(analyticValue-newMonteCarloValue)/analyticValue*100)+ "\n" );
+
+		//or: another way to simulate the Black-Scholes model, by giving a Brownian motion
+
+		// Generating a one-dimensional Brownian motion
+		final BrownianMotion brownianMotionForBlackScholes = new BrownianMotionFromMersenneRandomNumbers(
+				times, //the time discretization of the Brownian motion
+				1, // number of independent Brownian motions that we generate: this is a 2-dimensional Brownian Motion
+				numberOfSimulations,//number of simulated paths
+				newSeed //the seed that is needed to generate the Mersenne random numbers (see line 141 of BrownianMotionFromMersenneRandomNumbers)
+				);
+
+		/*
+		 * look at the class: it links together the model, i.e., the specification of the dynamics
+		 * of the underlying, and the process, i.e., the discretization of the paths.
+		 */
+		final AssetModelMonteCarloSimulationModel bsModelWithBrownianMotion = new MonteCarloBlackScholesModel(
+				initialPrice, riskFreeRate, volatility, brownianMotionForBlackScholes);
+
+		final double thirdMonteCarloValue = europeanOption.getValue(bsModelWithBrownianMotion);
+
+		System.out.println("B-S Monte Carlo value using the Brownian motion: " + FORMATTERPOSITIVE4.format(thirdMonteCarloValue)
+		+ "\n" + "Absolute percentage error: "
+		+ FORMATTERPOSITIVE4.format(Math.abs(analyticValue-thirdMonteCarloValue)/analyticValue*100) );
+
+
+	}
+}
