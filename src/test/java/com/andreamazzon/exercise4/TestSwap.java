222c92f7b4e8a8223ca4eb960093f8b494991ff1
Andrea Mazzon <mazzon@math.lmu.de>
2020-12-04 15:07:51 +0100

    Added a new small test we did during the lecture

--- src/test/java/com/andreamazzon/exercise4/TestSwap.java
+++ src/test/java/com/andreamazzon/exercise4/TestSwap.java
@@ -16,7 +16,7 @@ public class TestSwap {
 	@Test
 	public void testSwapRate() {
 		final double yearFraction = 0.5;
-		final double tolerance = 1E-15;
+
 		final double[] zeroCouponBondCurve = { 0.9986509108, 0.9949129829, 0.9897033769, 0.9835370208, 0.9765298116,
 				0.9689909565 };
 
@@ -28,8 +28,25 @@ public class TestSwap {
 		final double swapValue = swapCalculator.getSwapValue(parSwapRate, yearFraction);
 		System.out.println("The value of the swap for the par swap rate is " + swapValue);
 
+		final double tolerance = 1E-15;
+
 		// we test if this value is zero, with a tolerance equal to the machine precision
 		Assert.assertEquals(0, swapValue, tolerance);
+
+		//now we do the same for a tenure structure whose times steps are not constant
+		final double[] times = {0.5, 1, 1.5, 2, 3, 3.5};
+		final Swap newSwapCalculator = new SwapWithoutFinmath(times, zeroCouponBondCurve, true);
+
+		final double newParSwapRate = newSwapCalculator.getParSwapRate();
+		System.out.println("The new par swap rate is " + newParSwapRate);
+
+		final double newSwapValue = newSwapCalculator.getSwapValue(newParSwapRate);
+		System.out.println("The value of the new swap for the par swap rate is " + newSwapValue);
+
+		Assert.assertEquals(0, newSwapValue, tolerance);
+
+
+
 	}
 
 }
