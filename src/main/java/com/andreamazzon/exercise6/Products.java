54f798ef2330e5b2b6176973c211a8a8646899ba
Andrea Mazzon <mazzon@math.lmu.de>
2020-12-18 08:46:33 +0100

    Added a method to compute the Caplet value with Monte Carlo

--- src/main/java/com/andreamazzon/exercise6/Products.java
+++ src/main/java/com/andreamazzon/exercise6/Products.java
@@ -1,7 +1,13 @@
 package com.andreamazzon.exercise6;
 
 
+import net.finmath.exception.CalculationException;
 import net.finmath.functions.AnalyticFormulas;
+import net.finmath.montecarlo.assetderivativevaluation.AssetModelMonteCarloSimulationModel;
+import net.finmath.montecarlo.assetderivativevaluation.MonteCarloBlackScholesModel;
+import net.finmath.montecarlo.assetderivativevaluation.products.EuropeanOption;
+import net.finmath.time.TimeDiscretization;
+import net.finmath.time.TimeDiscretizationFromArray;
 
 /**
  * This class has methods computing the value of a caplet and of a quanto caplet.
@@ -41,6 +47,49 @@ public class Products {
 						liborVolatility,
 						fixingDate,
 						strike);
+	}
+
+
+	/**
+	 * This method calculates and return the value of a Caplet under the Black model, using a Monte Carlo
+	 * method.
+	 *
+	 * @param initialForwardLibor, i.e. L_0 = L(T_1,T_2;0)
+	 * @param liborVolatility, the volatility of the LIBOR process under the Black model
+	 * @param strike, the strike of the option
+	 * @param fixing, i.e. T_1
+	 * @param paymentDate, i.e. T_2
+	 * @param paymentDateDiscountFactor, i.e. P(T_2;0)
+	 * @param notional, i.e. N
+	 * @throws CalculationException
+	 */
+	public static double calculateCapletValueBlackModelWithMonteCarlo(double initialForwardLibor,
+			double liborVolatility,
+			double strike,
+			double fixingDate,
+			double paymentDate,
+			double paymentDateDiscountFactor,
+			double notional,
+			double timeStep,
+			int numberOfSimulations) throws CalculationException {
+
+		//we first get the number of the time steps of the time discretization. Note: it has to be int
+		final int numberOfTimeSteps = (int) ((int) fixingDate/timeStep);
+
+		final TimeDiscretization times = new TimeDiscretizationFromArray(0.0, numberOfTimeSteps, timeStep);
+		//we construct the simulation..
+		final AssetModelMonteCarloSimulationModel blackModel = new MonteCarloBlackScholesModel(
+				times, numberOfSimulations, initialForwardLibor, 0, liborVolatility);
+		//..and the object for the ruropean option
+		final EuropeanOption europeanOption = new EuropeanOption(fixingDate, strike);
+
+		final double periodLength = paymentDate - fixingDate;
+		/*
+		 * the discount factor is the bond maturing at the payment date; the LIBOR rate
+		 * has no drift because we are changing to a certain equivalent measure
+		 * under which it exhibits martingale dynamics.
+		 */
+		return notional * paymentDateDiscountFactor * periodLength * europeanOption.getValue(blackModel);
 	}
 
 	/**
--- src/test/java/com/andreamazzon/exercise6/TestForCapletAndQuantoCaplet.java
+++ src/test/java/com/andreamazzon/exercise6/TestForCapletAndQuantoCaplet.java
@@ -2,12 +2,13 @@ package com.andreamazzon.exercise6;
 import org.junit.Assert;
 import org.junit.Test;
 
+import net.finmath.exception.CalculationException;
 import net.finmath.functions.AnalyticFormulas;
 
 /**
- * This is a test class, checking if the Caplet value computed by the method
- * Products.calculateCapletValueBlackModel is equal, up to a given tolerance,
- * to the one computed by AnalyticFormulas.blackModelCapletValue in the finmath library.
+ * This is a test class, checking if the Caplet values computed by the method
+ * Products.calculateCapletValueBlackModel and Products.calculateCapletValueBlackModelWithMonteCarlo
+ * are equal, up to a given tolerance, to the one computed by AnalyticFormulas.blackModelCapletValue in the finmath library.
  *
  * @author Andrea Mazzon
  */
@@ -29,25 +30,51 @@ public class TestForCapletAndQuantoCaplet {
 	final double notional = 10000;
 
 	@Test
-	public void testCaplet() {
+	public void testCaplet() throws CalculationException {
 
-		//tolerance for assertEqual
-		final double tolerance = 1E-13;
+		//tolerances for assertEqual
+		final double toleranceForAnalytic = 1E-15;
+		final double toleranceForMonteCarlo = 1E-2;
+
+
+		//parameters for the Monte Carlo simulation
+		final double timeStep = 0.1;
+		final int numberOfSimulations = 1000000;
+
 		final double maturityOfTheOption = fixingDate;
 		final double periodLength = paymentDate - fixingDate;
 
+		//the benchmark value
 		final double finmathLibraryValue = notional * AnalyticFormulas.blackModelCapletValue(
 				initialForwardLibor, liborVolatility, maturityOfTheOption, strike, periodLength,
 				discountFactorAtMaturity);
 
 		System.out.println("Value of the caplet computed by the Finmath library: " + finmathLibraryValue + "\n");
 
-		final double ourValue = Products.calculateCapletValueBlackModel(initialForwardLibor, liborVolatility,
+		//the value computed using the analytic BS formula from the finmath library
+		final double ourAnalyticValue = Products.calculateCapletValueBlackModel(initialForwardLibor, liborVolatility,
 				strike, fixingDate, paymentDate, discountFactorAtMaturity, notional);
 
-		System.out.println("Value of the caplet computed by our method: " + ourValue + "\n");
+		System.out.println("Value of the caplet computed using the analytic formula for a call option: " + ourAnalyticValue + "\n");
 
-		Assert.assertEquals(finmathLibraryValue, ourValue, tolerance);
+		/*
+		 * we divide by the notional because otherwise the test could fail if the notional is too big. Another way
+		 * could be to look at the relative error
+		 */
+		Assert.assertEquals(finmathLibraryValue/notional, ourAnalyticValue/notional, toleranceForAnalytic);
+
+		//the value computed using the Monte Carlo method
+		final double ourMonteCarloValue = Products.calculateCapletValueBlackModelWithMonteCarlo(initialForwardLibor,
+				liborVolatility, strike, fixingDate, paymentDate, discountFactorAtMaturity, notional, timeStep, numberOfSimulations);
+
+		System.out.println("Value of the caplet computed using the Monte Carlo valuation of a call option: " + ourMonteCarloValue + "\n");
+
+		/*
+		 * we divide by the notional because otherwise the test could fail if the notional is too big. Another way
+		 * could be to look at the relative error
+		 */
+		Assert.assertEquals(finmathLibraryValue/notional, ourMonteCarloValue/notional, toleranceForMonteCarlo);
+
 
 	}
 
