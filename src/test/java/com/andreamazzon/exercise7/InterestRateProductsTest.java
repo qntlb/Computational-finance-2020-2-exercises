aebc4c7f75d07731d6e8028b96d1754f3f6b6c01
Andrea Mazzon <mazzon@math.lmu.de>
2021-01-08 09:07:13 +0100

    Added a test to see the dependence of price of the swaption on bonds

--- src/test/java/com/andreamazzon/exercise7/InterestRateProductsTest.java
+++ src/test/java/com/andreamazzon/exercise7/InterestRateProductsTest.java
@@ -33,8 +33,74 @@ public class InterestRateProductsTest {
 		System.out.println("Swaption value: " + FORMATTERREAL4.format(swaptionValue));
 		System.out.println();
 
-
-
+		//		//now we want to see how the price of the swaption reacts if the value of the second bond P(T_2;0) increases
+		//
+		//		double newValueForSecondBond = 0.93; //we start from 0.93: it must be bigger than P(T_3;0)
+		//
+		//		while (newValueForSecondBond  < 1) {//it must be smaller than P(T_1;0)
+		//			bondCurve[1] = newValueForSecondBond;
+		//			//new price of the swaption for the new value of P(T_2;0)
+		//			final double newSwaptionValueForDifferentSecondBond = InterestRateProducts.calculateSwaptionValueBlack(bondCurve, yearFraction,
+		//					swaptionStrike, notional, swapRateVolatility);
+		//
+		//			System.out.println("P(T_2;0)= " + FORMATTERREAL2.format(newValueForSecondBond)
+		//			+ " swaption value: " + FORMATTERREAL4.format(newSwaptionValueForDifferentSecondBond));
+		//			newValueForSecondBond += 0.01;//it increases by 0.01 at every iteration
+		//		}
+		//		System.out.println();
+		//
+		//		//Let's investigate a bit more..
+		//
+		//		/*
+		//		 * We have that our price, as a function of the initial value S_0 of the par swap rate, is
+		//		 * (P(T_1;0)-P(T_n;0))/S_0 BS(S_0,..)
+		//		 * and S_0 is decreasing with respect to P(T_2;0) (look at the formula, or how it is computed in
+		//		 * the method of SwapWithoutFinmath).
+		//		 * So we want to look at the behavior of  the function 1/x BS(x,..) with respect to x.
+		//		 * We have that
+		//		 * (1/xBS(x,..))'=1/x DeltaBS(x,..)-1/x^2BS(x,..)=1/x(DeltaBS(x,..)-1/x BS(x,..)).
+		//		 * We then have to check if DeltaBS(x,..) > 1/x BS(x,..) for our values.
+		//		 * In this case, the price of the swaption is increasing with respect to S_0 and then decreasing
+		//		 * with respect to P(T_2;0).
+		//		 */
+		//
+		//		final Swap mySwap = new SwapWithoutFinmath(yearFraction, bondCurve, true);
+		//		//we compute the par swap rate at 0
+		//		final double initialSwapRate = mySwap.getParSwapRate(yearFraction);
+		//
+		//		//price of the call option for our parameters, and our value of S_0
+		//		final double valueOfTheCall = AnalyticFormulas.blackScholesOptionValue(
+		//				initialSwapRate, 0.0, swaptionStrike, notional, swapRateVolatility);
+		//
+		//		final double valueOfTheCallDividedByInitial = valueOfTheCall/initialSwapRate;
+		//
+		//		//delta of the call option for our parameters, and our value of S_0
+		//		final double deltaOfTheCall = AnalyticFormulas.blackScholesOptionDelta(
+		//				initialSwapRate, 0.0, swaptionStrike, notional, swapRateVolatility);
+		//
+		//		//let's now investigate the behavior with respect to the value of P(T_1;0).
+		//		System.out.println("Call divided by initial value: " + FORMATTERREAL2.format(valueOfTheCallDividedByInitial)
+		//		+ ". Delta of the call: " + FORMATTERREAL2.format(deltaOfTheCall));
+		//
+		//		System.out.println();
+		//
+		//		bondCurve[1] = 0.95;
+		//
+		//		double newValueForFirstBond = 0.96;//it has to be bigger than P(T_2;0)
+		//
+		//		while (newValueForFirstBond < 1) {//but smaller than 1
+		//			bondCurve[0] = newValueForFirstBond;
+		//
+		//			final double newSwaptionValue = InterestRateProducts.calculateSwaptionValueBlack(bondCurve, yearFraction,
+		//					swaptionStrike, notional, swapRateVolatility);
+		//
+		//			System.out.println("P(T_1;0)= " + FORMATTERREAL2.format(newValueForFirstBond)
+		//			+ " swaption value: " + FORMATTERREAL4.format(newSwaptionValue));
+		//
+		//			newValueForFirstBond += 0.01;
+		//		}
+		//		System.out.println();
+		//
 		//parameters for the caplet and for the caplet in arrears
 		final double initialForwardLibor = 0.05;
 		final double liborVolatility = 0.3;
